231ec5c5154bf0d382a00e0d76558cb17e432c5a
DiriARG <matidirisio@gmail.com>
2026-08-18 17:23:21 -0300

    Inicialización del proyecto.

---
+++ src/main/java/com/ironempire/IronEmpireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ironempire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronEmpireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronEmpireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironempire/IronEmpireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironempire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronEmpireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

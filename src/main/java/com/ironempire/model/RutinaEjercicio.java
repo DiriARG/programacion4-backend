3517128b9ccbf2c1cc54e20974ea8970622e76a2
DiriARG <matidirisio@gmail.com>
2026-08-23 15:14:45 -0300

    Creación de entidad RutinaEjercicio.

---
+++ src/main/java/com/ironempire/model/RutinaEjercicio.java
@@ -0,0 +1,47 @@
+package com.ironempire.model;
+
+import com.ironempire.enums.DiaSemana;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rutina_ejercicio")
+public class RutinaEjercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "rutina_id", nullable = false)
+    private Rutina rutina;
+
+    @ManyToOne
+    @JoinColumn(name = "ejercicio_id", nullable = false)
+    private Ejercicio ejercicio;
+
+    @Column(nullable = false)
+    private Integer series;
+
+    @Column(nullable = false)
+    private Integer repeticiones;
+
+    @Column(precision = 5, scale = 2)
+    private BigDecimal peso;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "dia_semana", nullable = false)
+    private DiaSemana diaSemana;
+
+    @Column(nullable = false)
+    private Integer orden;
+}

0180890bfd5200ddae566b1a8c4451538b294d48
DiriARG <matidirisio@gmail.com>
2026-08-29 17:39:51 -0300

    Creación del DTO "UsuarioResponse".

---
+++ src/main/java/com/ironempire/dto/response/usuario/UsuarioResponse.java
@@ -0,0 +1,23 @@
+package com.ironempire.dto.response.usuario;
+
+import com.ironempire.enums.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioResponse {
+    
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String email;
+    private String telefono;
+    private Rol rol;
+    private Boolean activo;
+}

4dd73ad5a95ba229c56606098b5b586400618094
DiriARG <matidirisio@gmail.com>
2026-08-30 17:04:58 -0300

    Se agrega la configuración de Spring Security.

--- src/main/java/com/ironempire/config/SeguridadConfig.java
+++ src/main/java/com/ironempire/config/SeguridadConfig.java
@@ -2,10 +2,15 @@ package com.ironempire.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SeguridadConfig {
 
     @Bean
@@ -13,4 +18,23 @@ public class SeguridadConfig {
         // Acá se define a BCrypt como PasswordEncoder.
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Se desactiva "CSRF" porque la API está protegida mediante Tokens (JWT) y no
+                // mediante Cookies.
+                .csrf(csrf -> csrf.disable())
+
+                // Configura la API como "Stateless" (sin estado) para que el servidor no guarde sesiones en memoria.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Reglas de acceso a las rutas.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
 }

c4dbca2eba75abb38626082387661fd59951b928
DiriARG <matidirisio@gmail.com>
2026-09-06 15:27:46 -0300

    DTOs para iniciar sesión.

---
+++ src/main/java/com/ironempire/dto/request/autenticacion/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.ironempire.dto.request.autenticacion;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor 
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El formato del email no es válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String contrasenia;
+}

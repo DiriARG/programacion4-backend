0495edd983b1cdee7fe1bc72046ec357891e8092
DiriARG <matidirisio@gmail.com>
2026-09-08 14:17:18 -0300

    Creación del controlador para hacer login.

---
+++ src/main/java/com/ironempire/controller/autenticacion/LoginController.java
@@ -0,0 +1,26 @@
+package com.ironempire.controller.autenticacion;
+
+import com.ironempire.dto.request.autenticacion.LoginRequest;
+import com.ironempire.dto.response.autenticacion.LoginResponse;
+import com.ironempire.service.autenticacion.LoginService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request) {
+
+        LoginResponse response = loginService.login(request);
+
+        return ResponseEntity.ok(response);
+    }
+}

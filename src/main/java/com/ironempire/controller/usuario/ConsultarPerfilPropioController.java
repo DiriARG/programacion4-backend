54e7db1dd94449780ee4d2cc4e6a9b2303a8dd4d
DiriARG <matidirisio@gmail.com>
2026-09-14 13:14:17 -0300

    Controller para que todos los usuarios puedan ver su propio perfil.

---
+++ src/main/java/com/ironempire/controller/usuario/ConsultarPerfilPropioController.java
@@ -0,0 +1,37 @@
+package com.ironempire.controller.usuario;
+
+import com.ironempire.dto.response.usuario.UsuarioResponse;
+import com.ironempire.service.usuario.ConsultarUsuarioService;
+import lombok.RequiredArgsConstructor;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class ConsultarPerfilPropioController {
+
+    private final ConsultarUsuarioService consultarUsuarioService;
+
+    @GetMapping("/perfil")
+    // Para que pueda ser ejecutado por cualquiera de los cuatro roles autenticados.
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<UsuarioResponse> consultarPerfilPropio(Principal principal) {
+
+        /*
+         * "principal.getName()" devuelve el "subject" (sub) del JWT.
+         * En nuestro sistema, el subject contiene el email del usuario autenticado.
+         */
+        String email = principal.getName();
+
+        UsuarioResponse response = consultarUsuarioService.consultarPerfilPropio(email);
+
+        return ResponseEntity.ok(response);
+    }
+}

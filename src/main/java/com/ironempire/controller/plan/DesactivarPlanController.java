d235104fb5e8aaae5519c86c2b009f20ce3926c8
DiriARG <matidirisio@gmail.com>
2026-09-24 14:39:15 -0300

    Controladores para la desactivación y reactivación de un plan.

---
+++ src/main/java/com/ironempire/controller/plan/DesactivarPlanController.java
@@ -0,0 +1,29 @@
+package com.ironempire.controller.plan;
+
+import com.ironempire.dto.response.plan.PlanResponse;
+import com.ironempire.service.plan.GestionarEstadoPlanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/planes")
+@RequiredArgsConstructor
+public class DesactivarPlanController {
+
+    private final GestionarEstadoPlanService gestionarEstadoPlanService;
+
+    @PatchMapping("/{id}/desactivar")
+    @PreAuthorize("hasRole('ADMIN_GENERAL')")
+    public ResponseEntity<PlanResponse> desactivarPlan(
+            @PathVariable Long id) {
+
+        PlanResponse response = gestionarEstadoPlanService.desactivarPlan(id);
+
+        return ResponseEntity.ok(response);
+    }
+}
